b27909af985b7900dbbff484b00e6045e7bad131
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-12 05:53:21 +0000

    Upload results to S3

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var Q = require('q');
 var sqs = require('simple-queue-service');
 var aws = require('aws-sdk');
-var config;
-try { config = require('./config.json'); } catch (ex) {}
-var get = Q.nfbind(require('request').get);
 
 var join = require('path').join;
 var runJob = require('jepso-job-runner');
@@ -12,19 +9,32 @@ var express = require('express');
 var app = express();
 var server = module.exports = require('http').createServer(app);
 
+//load configuration
+var config;
+try { config = require('./config.json'); } catch (ex) {}
 var access = process.env.WORKER_ACCESS || config.access;
 var secret = process.env.WORKER_SECRET || config.secret;
 var region = process.env.WORKER_REGION || (config && config.region);
 var queueName = process.env.WORKER_QUEUE || config.queue;
 var reposTableName = process.env.WORKER_REPOS_TABLE || config['repos-table'];
 var s3BucketName = process.env.WORKER_S3_BUCKET || config['S3-bucket'];
+var topic = process.env.WORKER_SNS_TOPIC || config['notification-topic'];
 
 var sauceID = process.env.WORKER_SAUCE || config.sauce;
 var sauceKey = process.env.WORKER_SAUCE_KEY || config['sauce-key'];
+var sauceParallelization = process.env.WORKER_SAUCE_PARALLELIZATION || config['sauce-parallelization'] || 3;
 
-var emit = require('./lib/emit');
+var checkStatus = require('./lib/check-status');
+var emit = require('./lib/emit-event')(access, secret, region, topic);
+var markComplete = require('./lib/mark-build-complete')(access, secret, region, reposTableName);
+var s3FS = require('./lib/s3-fs')({
+  bucket: s3BucketName,
+  key: access,
+  secret: secret,
+  region: region
+});
 
-var throttle = require('throat')(2);
+var throttle = require('throat')(sauceParallelization);
 function sauce(fn) {
   return throttle(function () {
     return fn(sauceID, sauceKey);
@@ -33,17 +43,6 @@ function sauce(fn) {
 
 var queue = sqs(access, secret, region).createQueue(queueName, {visibilityTimeout: '10 minutes'});
 
-function checkStatus() {
-  return get('https://status.github.com/api/status.json')
-    .spread(function (res) {
-      if (res.statusCode != 200) return true; //assume it's just the status site that's offline
-      var status = JSON.parse(res.body.toString()).status;
-      if (status === 'good') return true;
-      var err = new Error('GitHub status is ' + status);
-      err.name = 'GitHubStatus';
-      throw err;
-    })
-}
 recieveMessage();
 function recieveMessage() {
   Q(queue.nextMessage())
@@ -74,16 +73,24 @@ function processMessage(message) {
   var tag = message.body.tag;
   var buildID = message.body.buildID;
   console.warn('begin: ' + user + '/' + repo + '/' + tag);
-  return runJob({
+  return Q(runJob({
     sauce: sauce,
     commit: {user: user, repo: repo, tag: tag},
     buildCreatedTime: Date.now(),
-    directory: join(__dirname, 'output', user, repo, buildID)
+    fileSystem: s3FS(user + '/' + repo + '/' + buildID)
   }, function (name, user, repo, data) {
-    //todo: keep message alive
-    return emit(name, user, repo, data);
-  });
-  //todo: Mark job as complete
+    //keep from message timeing out
+    return Q(message.extendTimeout('10 minutes'))
+      .then(function () {
+        return emit(name, user, repo, data);
+      });
+  }))
+  .then(function () {
+    return markComplete(user, repo, buildID);
+  })
+  .then(function () {
+    console.warn('end: ' + user + '/' + repo + '/' + tag);
+  })
 }
 
 server.listen(3001);
